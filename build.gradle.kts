f689f80f81c3d83b731309a2c369fb9e70e2e61a
IPECTER <ipectert@gmail.com>
2023-12-31 00:00:37 +0900

    Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.7-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.5.10"
 }
 
 repositories {
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.9:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.11:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
     paperclip("io.papermc:paperclip:3.0.3")
 }
@@ -43,6 +43,98 @@ allprojects {
 
             publications.register<MavenPublication>("gpr") {
                 from(components["java"])
+            }
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    tasks {
+        withType<JavaCompile>().configureEach {
+            options.compilerArgs.add("--add-modules=jdk.incubator.vector")
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(17)
+        }
+
+        withType<Javadoc> {
+            options.encoding = Charsets.UTF_8.name()
+        }
+
+        withType<ProcessResources> {
+            filteringCharset = Charsets.UTF_8.name()
+        }
+
+        withType<Test> {
+            minHeapSize = "2g"
+            maxHeapSize = "2g"
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+}
+
+paperweight {
+    serverProject.set(project(":plazma-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    usePaperUpstream(providers.gradleProperty("paperCommit")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("Plazma-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("Plazma-Server"))
+        }
+    }
+}
+
+tasks {
+    generateDevelopmentBundle {
+        apiCoordinates.set("org.plazmamc.plazma:plazma-api")
+        mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+        libraryRepositories.addAll(
+            "https://repo.maven.apache.org/maven2/",
+            "https://papermc.io/repo/repository/maven-public/"
+        )
+    }
+
+    register("updateUpstream") {
+        val tempDir = layout.cacheDir("updateUpstream");
+        val file = "gradle.properties";
+
+        doFirst {
+            val apiResponse = layout.cache.resolve("apiResponse.json");
+            download.get().download("https://api.github.com/repos/PaperMC/Paper/commits/master", apiResponse);
+            val latestCommit = gson.fromJson<paper.libs.com.google.gson.JsonObject>(apiResponse)["sha"].asString;
+
+            copy {
+                from(file)
+                into(tempDir)
+                filter { line: String ->
+                    line.replace("paperCommit = .*".toRegex(), "paperCommit = $latestCommit")
+                }
+            }
+        }
+
+        doLast {
+            copy {
+                from(tempDir.file("gradle.properties"))
+                into(project.file(file).parent)
             }
         }
     }
