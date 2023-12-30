7acb4f4d9dd42ce8aa6ea26f81834fa1e48c29b8
IPECTER <ipectert@gmail.com>
2023-12-30 23:51:42 +0900

    Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,84 +23,27 @@ dependencies {
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
-subprojects {
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-    
-    tasks {
-        withType<JavaCompile>().configureEach {
-            options.encoding = "UTF-8"
-            options.release.set(17)
-        }
-    
-        withType<Javadoc> {
-            options.encoding = Charsets.UTF_8.name()
-        }
-    
-        withType<ProcessResources> {
-            filteringCharset = Charsets.UTF_8.name()
-        }
-    
-        withType<Test> {
-            minHeapSize = "2g"
-            maxHeapSize = "2g"
-        }
-    }
 
-    repositories {
-        mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
+    publishing {
+        repositories {
+            maven {
+                name = "githubPackage"
+                url = uri("https://maven.pkg.github.com/PlazmaMC/Plazma")
 
-}
-
-paperweight {
-    serverProject.set(project(":plazma-server"))
-
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
-
-    usePaperUpstream(providers.gradleProperty("paperCommit")) {
-        withPaperPatcher {
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("Plazma-API"))
-
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("Plazma-Server"))
-        }
-    }
-}
-
-val upstreamTask = tasks.register("updateUpstream") {
-    val tempDir = layout.cacheDir("updateUpstream");
-    val file = "gradle.properties";
-
-    doFirst {
-        val apiResponse = layout.cache.resolve("apiResponse.json");
-        download.get().download("https://api.github.com/repos/PaperMC/Paper/commits/master", apiResponse);
-        val latestCommit = gson.fromJson<paper.libs.com.google.gson.JsonObject>(apiResponse)["sha"].asString;
-
-        copy {
-            from(file)
-            into(tempDir)
-            filter { line: String ->
-                line.replace("paperCommit = .*".toRegex(), "paperCommit = $latestCommit")
+                credentials {
+                    username = System.getenv("GITHUB_USERNAME")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
             }
-        }
-    }
 
-    doLast {
-        copy {
-            from(tempDir.file("gradle.properties"))
-            into(project.file(file).parent)
+            publications.register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
         }
     }
 }
