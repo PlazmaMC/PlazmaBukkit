3b27767e4b96d6ea4122b550dc44c854ab4b824e
AlphaKR93 <dev@alpha93.kr>
2024-01-15 18:50:12 +0900

    Use libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,21 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {}
+        create("api") {
+            from(files("libs/api.versions.toml"))
+        }
+        create("server") {
+            from(files("libs/server.versions.toml"))
+        }
+        create("common") {
+            from(files("libs/common.versions.toml"))
+        }
+    }
+}
+
 rootProject.name = "plazma"
 for (name in listOf("Plazma-API", "Plazma-Server")) {
     val projName = name.lowercase(Locale.ENGLISH)
