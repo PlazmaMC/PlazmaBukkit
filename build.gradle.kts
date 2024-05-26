33812e0bc84dc7aabd870cea32e1223de2a1d8ee
Alpha <dev@alpha93.kr>
2024-05-26 17:30:32 +0900

    :skull:
    Signed-off-by: Alpha <dev@alpha93.kr>

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ kotlin.jvmToolchain(jdkVersion)
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
