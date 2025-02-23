8b0a4c75c52aa86ccaf7bfb7b40b7ce246067a91
AlphaKR93 <dev@alpha93.kr>
2025-02-23 17:48:16 +0900

    Remove unnecessary client features

--- plazma-server/src/main/java/org/plazmamc/plazma/Options.java
+++ plazma-server/src/main/java/org/plazmamc/plazma/Options.java
@@ -4,6 +4,9 @@ import static java.lang.Boolean.getBoolean;
 
 public interface Options {
 
+    boolean ASYNC = getBoolean("Plazma.forciblyAllowAsyncAccess");
     boolean OPTIMIZE = !getBoolean("Plazma.disableConfigOptimization");
+    boolean NO_WARN = getBoolean("Plazma.iKnowWhatIAmDoing");
+    boolean NO_WARN_DEV = getBoolean("Plazma.iKnowWhatIAmDoingISwear");
 
 }
