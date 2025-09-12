7f5ec163bac4586904532005731bd25e1232ceb0
IPECTER <ipectert@gmail.com>
2025-09-13 07:22:49 +0900

    work in progress

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.17"
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+paperweight {
+    upstreams.register("purpur") {
+        repo = github("PurpurMC", "Purpur")
+        ref = providers.gradleProperty("purpurCommit")
+
+        patchFile {
+            path = "purpur-server/build.gradle.kts"
+            outputFile = file("plazma-server/build.gradle.kts")
+            patchFile = file("plazma-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "purpur-api/build.gradle.kts"
+            outputFile = file("plazma-api/build.gradle.kts")
+            patchFile = file("plazma-api/build.gradle.kts.patch")
+        }
+        patchRepo("paperApi") {
+            upstreamPath = "paper-api"
+            patchesDir = file("plazma-api/paper-patches")
+            outputDir = file("paper-api")
+        }
+        patchDir("purpurApi") {
+            upstreamPath = "purpur-api"
+            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
+            patchesDir = file("plazma-api/purpur-patches")
+            outputDir = file("purpur-api")
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 21
+        options.isFork = true
+        options.compilerArgs.addAll(listOf("-Xlint:-deprecation", "-Xlint:-removal"))
+        options.forkOptions.memoryMaximumSize = "4g"
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
+        }
+    }
+
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven("https://repo.purpurmc.org/snapshots") {
+                name = "plazma"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPlazmaVersion") {
+    doLast {
+        println(project.version)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,54 @@
+import java.util.Locale
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Plazma project directory is not a properly cloned Git repository.
+         
+         In order to build Plazma from source you must clone
+         the repository using Git, not download a code zip from GitHub.
+         
+         See https://github.com/PurpurMC/Purpur/blob/HEAD/CONTRIBUTING.md
+         for further information on building and modifying Purpur.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
+rootProject.name = "plazma"
+
+for (name in listOf("plazma-api", "plazma-server")) {
+    val projName = name.lowercase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
+
+optionalInclude("test-plugin")
+
+fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
+    val settingsFile = file("$name.settings.gradle.kts")
+    if (settingsFile.exists()) {
+        apply(from = settingsFile)
+        findProject(":$name")?.let { op?.invoke(it) }
+    } else {
+        settingsFile.writeText(
+            """
+            // Uncomment to enable the '$name' project
+            // include(":$name")
+
+            """.trimIndent()
+        )
+    }
+}
