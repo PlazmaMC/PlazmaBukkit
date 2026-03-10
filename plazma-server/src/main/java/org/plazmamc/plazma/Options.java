377459cec1ccb7e39591d205be5814555e515da7
IPECTER <ipectert@gmail.com>
2026-03-10 23:07:12 +0900

    Configurable max stack size

--- plazma-server/src/main/java/org/plazmamc/plazma/Options.java
+++ plazma-server/src/main/java/org/plazmamc/plazma/Options.java
@@ -1,9 +1,11 @@
 package org.plazmamc.plazma;
 
 import static java.lang.Boolean.getBoolean;
+import static java.lang.Integer.getInteger;
 
 public interface Options {
 
     boolean OPTIMIZE = !getBoolean("Plazma.disableConfigOptimization");
+    int MAX_STACK_SIZE = getInteger("Plazma.maxStackSize", 99);
 
 }
